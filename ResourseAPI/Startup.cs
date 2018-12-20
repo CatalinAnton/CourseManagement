2b0d94cd3db41ee011cb0e3001493045f82d25d6
Silviu Apuscasitei <Silviu.Apuscasitei@endava.com>
2018-12-20 23:02:10 +0200

    Bug fixed

--- ResourseAPI/Startup.cs
+++ ResourseAPI/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ResourseAPI.Services;
 
 namespace ResourseAPI
 {
@@ -25,6 +27,7 @@ namespace ResourseAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ResourseService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
