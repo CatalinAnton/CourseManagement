98cafd11650a27580b747c6465dd59319a80d8e3
Feciuc Stelian <stelian@teramind.co>
2019-01-10 21:34:02 +0200

    Changed ID type in Resourse API.

--- ResourseAPI/Models/Resourse.cs
+++ ResourseAPI/Models/Resourse.cs
@@ -1,22 +1,23 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-using System.ComponentModel.DataAnnotations;
+
 
 namespace ResourseAPI.Models
 {
     public class Resourse
     {
-
-        public ObjectId _id { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
 
         [BsonElement("Type")]
-        [Required(ErrorMessage = "Type is required")]
         public string Type { get; set; }
 
         [BsonElement("Link")]
-        [Required(ErrorMessage = "Link is required")]
         public string Link { get; set; }
-
-        public ObjectId CourseId { get; }
+    
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CourseId { get; }
     }
 }
